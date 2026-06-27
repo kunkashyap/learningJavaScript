813430a0c790597714f54a02f710fa002fbf6712
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-27 18:28:58 +0530

    learning typecasting in JS

--- Datatypes02.js
+++ Datatypes02.js
@@ -1,7 +1,27 @@
-let score = "33"
+let score = false
 console.log(typeof score);
 
 
-//Type-Casting
+//Type-Casting (conversion of data-types)
 let valueInNumber = Number(score)
+
 console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//Notes
+/*
+"33" => 33
+"33abc" => NaN (not a number) typeof NaN is number
+true => 1; false => 0
+ */
+console.log("*****************")
+let isloggedIn = "kunal"
+let booleanisLoggedIn = Boolean(isloggedIn)
+
+console.log(typeof isloggedIn);
+console.log(booleanisLoggedIn)
+console.log(typeof booleanisLoggedIn);
+
+// 1 => true ; 0 => false
+// "" (empty string ) => false
+// "kunal" => true
