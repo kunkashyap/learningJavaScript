85077a782c33056abadd2f20b0701e6a86330b40
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-27 17:33:26 +0530

    data-types

---
+++ Datatypes_ECMA.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) => this is for Browser, not node.js
+
+console.log(3+3)
+console.log("KUNAL")
+ //Data Types
+
+let name = "kunal"
+let age = 19
+let isloggedIn = false
+
+let bigInt = 9007199254740993n
