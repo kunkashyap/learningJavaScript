bc3299bc9000aa12635b6b6dc82a4cc7a818af7a
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-27 12:10:05 +0530

    const,let and var

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 console.log("Kunal Kashyap");
 
+//const -> Once declared, value cannot be changed (immutable)
 //
+// const number = 11;
+/* console.log(number);  => This will throw an error because number is
+already declared
+ */
+
+
+
+
+const accountId = 33545;
+let accountEmail = "kunalkashyap@xyz.com";
+var accountPassword = "12345678";
+accountCity = "Delhi";
+
+// accountId = 2; Throw an error
+console.log(accountId);
+
+accountEmail = "kashyap.kunal@xyz.com";
+console.log(accountEmail);
+
+
+accountPassword = "abc201010#$@";
+console.log(accountPassword);
+
+accountCity = "Mussorie";
+//console.table => tabular form view
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+
