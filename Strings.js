6bd04e2b98848e12b13bc4fadcd4011690f8a4b6
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-28 23:29:55 +0530

    string methods

--- Strings.js
+++ Strings.js
@@ -15,4 +15,15 @@ console.log(gameName[3])
 
 //Methods For String
 
+console.log(gameName.__proto__)
+
 console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+
+
+//To see which character is at the given index
+console.log(gameName.charAt(3))
+
+//To see index of the given Character
+console.log(gameName.indexOf('t')) //=> Return -1 bcs 't' doesn't exist in gameName
