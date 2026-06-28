60fe4ab3bf6b821442a15aee90eff2f9d7889968
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-28 12:31:43 +0530

    comparisions

---
+++ DatatypeComparisions.js
@@ -0,0 +1,22 @@
+// console.log(3>=3)
+// console.log(2!=1)
+// console.log(2<4)
+// console.log(3==3)
+// console.log(3>=3)
+
+
+// console.log("2">1) //Automatically converted into number 2 from string
+// console.log("02">1)
+
+// console.log(null >= 0)
+// console.log(null == 0)
+// console.log(null < 0)
+//
+// console.log(undefined == 0)
+// console.log(undefined == 1)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+//Strict Check === (checks datatype doesn't convert)
+console.log("2" === 2)
+console.log("3" === "3")
