3210af86e1580e784c68ca464381a460ef8d7e7c
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-28 23:22:52 +0530

    meow

---
+++ Strings.js
@@ -0,0 +1,18 @@
+const name = "Kunal"
+const repoCount = 50
+
+//Outdated
+// console.log(name + " " + repoCount)
+
+//String Interpolation
+console.log(`Hi, My name is ${name} and my Github repos is around ${repoCount}`)
+
+
+const gameName = new String('KunalKK') //Creates an Object
+console.log(gameName)
+//Index of a string
+console.log(gameName[3])
+
+//Methods For String
+
+console.log(gameName.length)
