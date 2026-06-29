1b9e26c7a6a465cc1176b856fd88ccdc235afb50
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-29 15:45:36 +0530

    maths and numbers

---
+++ NumberAndMaths.js
@@ -0,0 +1,18 @@
+const score = 400
+
+const balance = new Number(100);
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2 ))
+
+const otherNumber = 232.49911
+console.log(otherNumber.toPrecision(5))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+console.log("***************MATHS*****************")
+
+
+console.log(Math.random())
